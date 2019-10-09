d7304eb4d2c8ea794b7a5bca2782579f6c183ca7
Tyler Nichols <tnicho44@slcc.edu>
2019-10-09 16:47:22 -0600

    Nav working

--- src/bootstrapElements/Navbar.js
+++ src/bootstrapElements/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import {Nav, Navbar, NavDropdown, MenuItem, Tabs, ButtonToolbar, Button, Table, ButtonGroup, Row, Col, Grid, Panel, FormGroup, FormControl} from 'react-bootstrap';
 
 function Navigator (){
     return(
-    <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar bg="dark" expand="lg">
+  <Navbar.Brand href="#home">Go Friend Finder</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+      <Nav.Link href="#home">Groups</Nav.Link>
+      <Nav.Link href="#link">Account</Nav.Link>
       </Nav>
       </Navbar.Collapse>
 </Navbar>
