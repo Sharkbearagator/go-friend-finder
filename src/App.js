560693614b2dbca67a591490232e199dc6ba64dc
Tyler Nichols <tnicho44@slcc.edu>
2019-10-11 16:11:48 -0600

    added groups page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Homepage";
 import Log from "./Pages/Login";
 import Acc from "./Pages/Account";
+import Group from "./Pages/Groups";
 const App = () => (
   <Router>
     <div>
@@ -10,6 +11,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Log} />
         <Route exact path="/account" component={Acc} />
+        <Route exact path="/groups" component={Group} />
       </Switch>
     </div>
   </Router>
