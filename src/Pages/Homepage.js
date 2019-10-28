a1b8f7906903c94514a7ea17fc946edb4153eedb
Tyler Nichols <tnicho44@slcc.edu>
2019-10-28 13:54:06 -0600

    fixed some react routing

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import {Nav, Navbar, Button, Jumbotron,} from 'react-bootstrap';
+import React from "react";
+import { Nav, Navbar, Button, Jumbotron } from "react-bootstrap";
 
-function Home (){
-    return(
-      <div>
-      {/* <Navbar bg="dark" variant="dark">
-  <Navbar.Brand href="#home">Go Friend Finder</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#home">Groups</Nav.Link>
-      <Nav.Link href="#link">Account</Nav.Link>
-      </Nav>
-      </Navbar.Collapse>
-</Navbar> */}
-        <Jumbotron>
+function Home() {
+  return (
+    <div>
+      <Jumbotron>
         <h1>Hello, Trainers!</h1>
         <p>
-          Welcome to Go Friend Finder! A place where you can find all the people that love GO as much as you do.
-          Create your account here or login with Google, Facebook, or your account.
+          Welcome to Go Friend Finder! A place where you can find all the people
+          that love GO as much as you do. Create your account here or login with
+          Google, Facebook, or your account.
         </p>
         <p>
-    <Button variant="primary" a href="/login">Login</Button>
-    <Button variant="primary">Create account</Button>
+          <Button variant="primary" a href="/login">
+            Login
+          </Button>
+          <Button variant="primary">Create account</Button>
         </p>
-        
       </Jumbotron>
-      </div>
-    )
+    </div>
+  );
 }
 
 export default Home;
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,22 +4,9 @@ import { Nav, Navbar, Button, Jumbotron, Form } from "react-bootstrap";
 function Log() {
   return (
     <div>
-      {/* <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Go Friend Finder</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/groups">Groups</Nav.Link>
-            <Nav.Link href="/account">Account</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar> */}
       <Jumbotron>
         <h1>Hello, Returning Trainer!</h1>
-        <p>
-        Sign in with facebook, google, or your Go Friend Finder account
-        </p>
+        <p>Sign in with facebook, google, or your Go Friend Finder account</p>
         <p>
           <Form>
             <Form.Group controlId="formBasicEmail">
@@ -34,11 +21,11 @@ function Log() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-            <Button variant="primary" type="submit" >
-            <i class="fab fa-facebook-f"></i>
+            <Button variant="primary" type="submit">
+              <i class="fab fa-facebook-f"></i>
             </Button>
             <Button variant="primary" type="submit">
-            <i class="fab fa-google"></i>
+              <i class="fab fa-google"></i>
             </Button>
             <Button variant="primary" type="submit">
               Submit
